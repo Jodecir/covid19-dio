5a468cbc032d9d26d1bbfe1f7c1f4e79fdef4548
Jodecir Neto <jodecirneto@gmail.com>
2021-04-21 10:55:37 -0300

    panel spacing fixed

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -54,14 +54,23 @@ function Panel({ updateAt, onChange, data, country, getCoviddata }) {
     <Card>
       <CardPanelContentStyled>
         <div>
-          <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
-          <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
+          <Typography 
+            style={{
+              marginLeft: '28px',
+              fontSize: '24px',
+              fontWeight: '600'
+            }}
+            variant="h5" component="span" color="primary">COVID19
+          </Typography>
           <div className="pt-2">
             <Select onChange={onChange} value={country}>
               {COUNTRIES.map(renderCountries)}
             </Select>
           </div>
+        </div>
+        <div>
+          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography><br></br>
+          <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
         </div>
         {navigatorHasShare ? renderShareButton : renderCopyButton}
       </CardPanelContentStyled>
